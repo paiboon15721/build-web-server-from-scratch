5f7bd9abf8b005b4d74d55d0580badd075aa02ba
Paiboon Auengkongkatong <paiboon.a@cdg.co.th>
2018-04-01 19:57:00 +0700

    add comment

--- 01/main.go
+++ 01/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"io"
-	"log"
 	"net"
 )
 
 func main() {
+	// Listen ที่ localhost port 8080
 	li, err := net.Listen("tcp", ":8080")
+
+	// ถ้าหากมี error ให้จบโปรแกรม
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
+
+	// code จะหยุดรอที่บันทัดนี้ จนกว่าจะมี client request เข้ามา
 	conn, err := li.Accept()
+
+	// ถ้าหากมี error ให้จบโปรแกรม
 	if err != nil {
-		log.Println(err)
+		panic(err)
 	}
+
+	// write string กลับไปที่ connection ที่ request เข้ามา
 	io.WriteString(conn, "This message from TCP server")
 }
